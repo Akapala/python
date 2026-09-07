30b4a78c8f035712d42dee27e33bb167c3ed3d32
Li <>
2026-09-07 14:00:55 +0800

    条件循环

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,14 +1,12 @@
-import turtle
-
-turtle.pensize(4)
-turtle.pencolor('red')
-
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-
-turtle.mainloop()
+LEVEL = 8
+star = 1
+blank = 0
+full_fill = 1+2*(LEVEL-1)
+for i in range(LEVEL):
+    blank = (full_fill - star)/2
+    for j in range(int(blank)):
+        print(" ", end="")
+    for j in range(star):
+        print("*", end="")
+    print("\n")    
+    star += 2
